41ffb169d2091e6a1f10c53ad814bd8b237bfeae
basementcz <you@example.com>
2018-12-29 17:02:15 +0100

    Update

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var app = express();
 
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
